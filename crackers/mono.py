60c471926252b99fdb8bbe8d084aaa0604b45ac1
Dita Hollmannová <hollmdit@fel.cvut.cz>
2019-03-05 23:37:26 +0100

    Affine ciphers are now solved automatically

--- crackers/mono.py
+++ crackers/mono.py
@@ -9,7 +9,19 @@ def crack_mono(stats, dictionary):
         solution = rot(stats.text, shift)
         # print(solution)
         score = compute_score(solution, dictionary)
-        solutions.append((score, solution))
+        solutions.append((score, solution, "rot"+str(shift)))
+
+    # flip (A=Z, B=Y...)
+    solution = flip_substitution(stats.text)
+    score = compute_score(solution, dictionary)
+    solutions.append((score, solution, "flip"))
+
+    # affine ciphers
+    for a, a_inv in zip([1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25],[1, 9, 21, 15, 3, 19, 7, 23, 11, 5, 17, 25]):
+        for b in range(1, 26):
+            solution = affine(stats.text, a_inv, b)
+            score = compute_score(solution, dictionary)
+            solutions.append((score, solution, "affine"+str(a)+","+str(b)))
 
     solutions.sort(key=lambda solution: solution[0], reverse=True)
     print(solutions[0][1])
@@ -29,6 +41,14 @@ def flip_substitution(cyphertext):
         letter_pos = ord(c) - 65
         letter_pos = abs(letter_pos - 25) + 65
         cleartext += chr(letter_pos)
+    return cleartext
+
+
+def affine(cyphertext, a_inv, b):
+    cleartext = ""
+    for c in cyphertext:
+        cl = chr((a_inv*(ord(c)-65-b) % 26) + 65)
+        cleartext += cl
     return cleartext
 
 
